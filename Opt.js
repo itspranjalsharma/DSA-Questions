c8a8e6f15575b5415fbf697cd92de2d4abbe69f9
Pranjalps5 <pranjalsharmaps5@gmail.com>
2024-03-24 23:34:12 +0530

    opt

---
+++ Opt.js
@@ -0,0 +1,8 @@
+function generateotp(length) {
+  let otp = "";
+  for (let i = 0; i < length; i++) {
+    otp += Math.floor(Math.random() * 10).toString;
+  }
+  return otp;
+}
+console.log(generateotp(3));
