47f6550df66a991e7cd7ce48d3d375e95536960d
Pranjalps5 <pranjalsharmaps5@gmail.com>
2024-05-13 23:41:05 +0530

    sum shortes program

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,9 @@
-  return [];
+// Sum the elements of an array
+
+let arr = [1, 2, 3, 4, 10];
+
+// we have to sum the give array elements
+
+let sum = arr.reduce((a, b) => a + b, 0);
+
+console.log(sum);
