1ff73e4c06df6901ed6ef2478a9a78b7b42e131f
Pranjalps5 <pranjalsharmaps5@gmail.com>
2024-05-22 23:52:12 +0530

    // findRectangleDetails function

--- reverse.js
+++ reverse.js
@@ -16,9 +16,34 @@ function reverseFunction(num) {
     reversedStr += numStr[i];
   }
   return parseInt(reversedStr);
-}
+}N
+
 
 let num1 = 987654321;
 let reversedNum = reverseFunction(num);
 
 console.log(reversedNum);
+
+
+
+// findRectangleDetails function 
+ 
+function findRectangleDetails(perimeter, length) {
+  // Check for invalid input (perimeter cannot be less than twice the length)
+  if (perimeter < 2 * length) {
+    console.error("Invalid input: Perimeter cannot be less than twice the length");
+    return;
+  }
+
+  const breadth = (perimeter - 2 * length) / 2;
+  const area = length * breadth;
+
+  console.log("Breadth of the rectangle:", breadth, "cm");
+  console.log("Area of the rectangle:", area, "cm²");
+}
+
+// Example usage:
+const perimeter = 230;
+const length = 70;
+
+findRectangleDetails(perimeter, length);
