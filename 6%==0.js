6ab66fb40eb045b10f5712ea49ed141c8d05f6ca
Pranjalps5 <pranjalsharmaps5@gmail.com>
2024-05-27 23:49:40 +0530

    divisiblabilty

---
+++ 6%==0.js
@@ -0,0 +1,14 @@
+// JavaScript code for the above approach
+// To check whether the given number is divisible by 6 or not
+
+//input  
+var r = 874654968;
+
+// finding given number is divisible by 6 or not 
+if (r % 6 === 0) {
+  console.log("Yes, it's divisble by 6");
+} else {
+  console.log("Sorry, it is not the one....");
+}
+// This code is contributed by Pranjal Sharma
+
