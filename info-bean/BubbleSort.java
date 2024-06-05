ec943bfaf9a1848bab730ad18701c1fe661d51ac
Pranjalps5 <pranjalsharmaps5@gmail.com>
2024-06-05 23:49:41 +0530

    bubble.java

---
+++ info-bean/BubbleSort.java
@@ -0,0 +1,46 @@
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Unsorted array: ");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.println("\nSorted array: ");
+        printArray(arr);
+    }
+
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                    swapped = true;
+                }
+            }
+
+            // Optimization: If no elements were swapped in the inner loop, the array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
