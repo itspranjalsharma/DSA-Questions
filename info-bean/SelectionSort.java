5fea7b21f9b7b5c56b8a2c23280db8097571f66c
Pranjalps5 <pranjalsharmaps5@gmail.com>
2024-06-07 00:04:52 +0530

    SelecationSort

---
+++ info-bean/SelectionSort.java
@@ -0,0 +1,35 @@
+public class SelectionSort {
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap the found minimum element with the first element of unsorted array
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        System.out.println("Before sorting: ");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.println("After sorting: ");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
