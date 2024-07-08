724a8e9c559b2c5c3cc82553624e27145f16f4f2
Pranjalps5 <pranjalsharmaps5@gmail.com>
2024-07-08 20:12:08 +0530

    0

--- isVowel.js
+++ isVowel.js
@@ -22,11 +22,18 @@ console.log("<br>");
 
 // JavaScript program to count vowels in a string
 
-// Function to check the Vowel
-function isVowel(ch) {
-  ch = ch.toUpperCase();
-  return ch == "A" || ch == "E" || ch == "I" || ch == "O" || ch == "U";
-}
+/**
+ * This program checks whether a given character is a vowel or not.
+ * @param {string} ch - The character to be checked.
+ * @returns {boolean} True if the character is a vowel, false otherwise.
+ *
+ * The function first converts the character to uppercase using the `toUpperCase` method.
+ * Then it checks if the character is equal to any of the five vowels 'A', 'E', 'I', 'O', 'U'.
+ * If it is, the function returns true. If it is not, the function returns false.
+ *
+ * This function is used in the `countOfVowel` and `countVowels` functions to count the
+ * number of vowels in a string.
+ */
 
 // Returns count of vowels in str
 function countVowels(str) {
--- prime.js
+++ prime.js
@@ -1,15 +1,25 @@
-function isPrime(n) {
-  if (n <= 1) {
-    return false;
-  }
+// function isPrime(n) {
+//   if (n <= 1) {
+//     return false;
+//   }
 
-  // Check if prime is or not
-  for (var i = 2; i < n; i++) {
-    if (n % i == 0) {
-      return false;
-    }
-  }
-  return true;
-}
+//   // Check if prime is or not
+//   for (var i = 2; i < n; i++) {
+//     if (n % i == 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
-isPrime(18) ? console.log("true") : console.log("false");
+// isPrime(18 ) ? console.log("true") : console.log("false");
+
+/**
+ * This program finds the largest number in the array [1, 2, 3, 4, 5, 6, 7, 8]
+ * and stores it in the variable `eight`.
+ * 
+ * It then iterates through each element of the array and if an element is greater
+ * than the current value of `eight`, it updates `eight` with the new value.
+ * 
+ * Finally, it logs the value of `eight` to the console.
+ */
