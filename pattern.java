26bcfdfaa5034f7999d5941374cbbaceb947daf6
Pranjal Sharma <109729802+itspranjalsharma@users.noreply.github.com>
2024-10-27 20:15:55 +0530

    Update pattern.java
    *
    **
    ***
    ****
    *****
    ----> n time.

--- pattern.java
+++ pattern.java
@@ -1,3 +1,12 @@
 class pattern {
+public static void main (String args []){
+ int n = 6;
+  for (int i =1;i<=n;i++){
+  for (int j=i;j<=i;j++){
+System.out.print("*");
+}
+System.out.println();
+  }
 
+}
 }
